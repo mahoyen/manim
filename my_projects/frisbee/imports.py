f949871aeca3fe4f1cb7a90ffded1b05ad891086
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2020-03-19 09:17:18 +0000

    added imports file

---
+++ my_projects/frisbee/imports.py
@@ -0,0 +1,7 @@
+
+from my_projects.frisbee.frisbeeConstants import *
+from my_projects.frisbee.frisbeeDisc import *
+from my_projects.frisbee.frisbeePitch import *
+from my_projects.frisbee.frisbeeTeam import *
+from my_projects.frisbee.frisbeePlayer import *
+
