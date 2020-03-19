5edb03ce03323acc4babf5351da4a1dbb756e8c2
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2020-03-19 17:11:40 +0000

    propper movement achieved

--- my_projects/playtesting/dotMovement.py
+++ my_projects/playtesting/dotMovement.py
@@ -15,8 +15,23 @@ class DotMovement(Scene):
         )
         self.wait()
 
-        dotlist[1].target = Dot(LEFT, color=PURPLE)
+        # dotlist[1].target = Dot(LEFT, color=PURPLE)
+        x_trans, y_trans, z_trans  = -5, 2, 0
+        theta = np.deg2rad(40)
+        
+
+        def apply_rotTransMatrix(x, y, z, t):
+            rotTransMatrix = np.array(
+            [[np.cos(theta), -np.sin(theta), 0, t*x_trans],
+             [np.sin(theta),  np.cos(theta), 0, t*y_trans],
+             [            0,              0, 1, t*z_trans]]
+        )
+            vector = np.array([x, y, z, 1])
+            return np.matmul(rotTransMatrix, vector)
+
+
+        #dotlist[1].apply_function(lambda x: apply_rotTransMatrix(*x, rotTransMatrix))
         self.play(
-            MoveToTarget(dotlist[1])
+            Homotopy(apply_rotTransMatrix, dotlist[0])
         )
         self.wait(5)
