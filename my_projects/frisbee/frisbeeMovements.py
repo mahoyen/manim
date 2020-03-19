3ddb27f12270bdc440fbd6f4546ae48819254fa5
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2020-03-19 12:45:00 +0000

    stuff for later

---
+++ my_projects/frisbee/frisbeeMovements.py
@@ -0,0 +1,7 @@
+ # def func(t):
+        #     return np.array((
+        #     -currentPlayer.direction[0]*FRISBEE_DEFAULT_SCALE*ENDZONE_MID*(1-t),
+        #     currentPlayer.get_position()[1]*(np.exp(t-1)-1),
+        #     0
+        #     ))
+        # onepath = ParametricFunction(func)
