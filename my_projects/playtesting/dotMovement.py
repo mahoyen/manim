c99dfe009bcb1b68a60822eb921acd73ffb16850
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2020-03-19 18:15:38 +0000

    removed unnecessary things, and added animation list comprehension

--- my_projects/playtesting/dotMovement.py
+++ my_projects/playtesting/dotMovement.py
@@ -16,26 +16,18 @@ class DotMovement(Scene):
         )
         self.wait()
 
-        # dotlist[1].target = Dot(LEFT, color=PURPLE)
-        x_trans, y_trans, z_trans  = -5, 2, 0
-        theta = np.deg2rad(40)
+        poslist = [DR, DL, UR]
         
-
-        def apply_rotTransMatrix(x, y, z, t):
-            rotTransMatrix = np.array(
-            [[np.cos(theta), -np.sin(theta), 0, t*x_trans],
-             [np.sin(theta),  np.cos(theta), 0, t*y_trans],
-             [            0,              0, 1, t*z_trans]]
-        )
-            vector = np.array([x, y, z, 1])
-            return np.matmul(rotTransMatrix, vector)
         tri = Triangle()
         self.add(tri)
         #dotlist[1].apply_function(lambda x: apply_rotTransMatrix(*x, rotTransMatrix))
         self.play(
-            MoveToPoint(DL, dotlist[0]),
             MoveToPoint(UR, dotlist[0]),
             MoveToPoint(3*UL, dotlist[2]),
             MoveToPointAndRotate(2*DR,30, tri),
+        )
+        self.wait()
+        self.play(
+            *[MoveToPoint(pos, dotitem) for pos, dotitem in zip(poslist, dotlist)],
         )
         self.wait(5)
