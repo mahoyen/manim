5067c006614f5945c89c79318bf27c791bd0a4f1
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2020-03-21 10:53:27 +0000

    added cutter mark positioning

--- my_projects/frisbee/frisbeeUtils.py
+++ my_projects/frisbee/frisbeeUtils.py
@@ -1,6 +1,6 @@
 import numpy as np
 from manimlib.constants import Z_AXIS, ORIGIN
-from my_projects.frisbee.frisbeeConstants import HANDLER, FLICK, HANDLER_MARK_DISTANCE_VSTACK
+from my_projects.frisbee.frisbeeConstants import *
 
 def calculate_relative_mark_position_vstack(playerRole, markForce, markDirection):
     forceSide = 1 if markForce == FLICK else -1
@@ -8,4 +8,6 @@ def calculate_relative_mark_position_vstack(playerRole, markForce, markDirection
         return HANDLER_MARK_DISTANCE_VSTACK*(forceSide*np.cross(markDirection, Z_AXIS)-markDirection)
     elif playerRole == DUMP:
         return DUMP_MARK_DISTANCE_VSTACK*(np.sin(DUMP_MARK_ANGLE_VSTACK)*forceSide*np.cross(markDirection, Z_AXIS)-markDirection)
+    else:
+        return CUTTER_MARK_DISTANCE_VSTACK*(-forceSide*np.cross(markDirection, Z_AXIS)+markDirection)
     return ORIGIN
