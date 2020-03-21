81cc072beecf9d6672e2b21a810215270ef7ba68
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2020-03-21 11:02:42 +0000

    added angling of mark

--- my_projects/frisbee/frisbeeUtils.py
+++ my_projects/frisbee/frisbeeUtils.py
@@ -5,9 +5,9 @@ from my_projects.frisbee.frisbeeConstants import *
 def calculate_relative_mark_position_vstack(playerRole, markForce, markDirection):
     forceSide = 1 if markForce == FLICK else -1
     if playerRole == HANDLER:
-        return HANDLER_MARK_DISTANCE_VSTACK*(forceSide*np.cross(markDirection, Z_AXIS)-markDirection)
+        return HANDLER_MARK_DISTANCE_VSTACK*(np.sin(HANDLER_MARK_ANGLE_VSTACK)*forceSide*np.cross(markDirection, Z_AXIS)-np.cos(HANDLER_MARK_ANGLE_VSTACK)*markDirection)
     elif playerRole == DUMP:
-        return DUMP_MARK_DISTANCE_VSTACK*(np.sin(DUMP_MARK_ANGLE_VSTACK)*forceSide*np.cross(markDirection, Z_AXIS)-markDirection)
+        return DUMP_MARK_DISTANCE_VSTACK*(np.sin(DUMP_MARK_ANGLE_VSTACK)*forceSide*np.cross(markDirection, Z_AXIS)-np.cos(DUMP_MARK_ANGLE_VSTACK)*markDirection)
     else:
-        return CUTTER_MARK_DISTANCE_VSTACK*(-forceSide*np.cross(markDirection, Z_AXIS)+markDirection)
+        return CUTTER_MARK_DISTANCE_VSTACK*(np.sin(CUTTER_MARK_ANGLE_VSTACK)*(-forceSide)*np.cross(markDirection, Z_AXIS)+np.cos(CUTTER_MARK_ANGLE_VSTACK)*markDirection)
     return ORIGIN
