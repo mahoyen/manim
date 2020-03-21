5f62456deb8487db90e44b85decdaf4834f0a2ea
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2020-03-21 11:30:43 +0000

    added labels and titles

--- my_projects/frisbee/frisbeeMovingTest.py
+++ my_projects/frisbee/frisbeeMovingTest.py
@@ -7,27 +7,32 @@ class MovementTest(Scene):
     def construct(self):
         frisbeePitch = FrisbeePitch()
         redTeam = FrisbeeTeam(LEFT, "offense", color=RED)
-        blueTeam = FrisbeeTeam(RIGHT, "defense", color=BLUE)
+        offenseLabel = TextMobject("Offense Team")
+        offenseLabel.to_corner(UL)
 
+        blueTeam = FrisbeeTeam(RIGHT, "defense", color=BLUE)
+        defenseLabel = TextMobject("Defense Team")
+        defenseLabel.to_corner(UR)
+
+        ODLabelVGroup = VGroup(offenseLabel, defenseLabel)
+
+        title = TextMobject("Vertical Stack Formation")
+        title.to_edge(UP)
         self.add(
             frisbeePitch.pitch,
         )
         self.play(
+            FadeIn(ODLabelVGroup),
             FadeInFromPoint(blueTeam.players, LEFT_SIDE),
             FadeInFromPoint(redTeam.players, RIGHT_SIDE),
         )
         self.wait()
 
-        redTeam.vstack_formation(0, BRICK_MARK_POSITION*-redTeam.direction*FRISBEE_DEFAULT_SCALE)
-        blueTeam.start_formation(ORIGIN)
+        blueTeam.vstack_formation(0, BRICK_MARK_POSITION*-blueTeam.direction*FRISBEE_DEFAULT_SCALE)
+        redTeam.mark_players(blueTeam)
         self.play(
+            Write(title, run_time=3),
             *[MoveToPoint(player.destination, player) for player in redTeam.players],
-            *[MoveToPoint(player.destination, player) for player in blueTeam.players],
-        )
-        
-        blueTeam.mark_players(redTeam)
-        
-        self.play(
             *[MoveToPoint(player.destination, player) for player in blueTeam.players],
         )
         self.wait(5)
